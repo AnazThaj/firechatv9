6a78e1609c96feb581a7f633952781307ad5a303
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-27 02:58:15 +0530

    Add firebase config code

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ7_ZkyotSspPHUZctwcDD_Uo26G99vXU",
+  authDomain: "fire-kangaroo-chat-app-test.firebaseapp.com",
+  projectId: "fire-kangaroo-chat-app-test",
+  storageBucket: "fire-kangaroo-chat-app-test.appspot.com",
+  messagingSenderId: "43617576246",
+  appId: "1:43617576246:web:f7cc9cf393c2cc02713f97",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
