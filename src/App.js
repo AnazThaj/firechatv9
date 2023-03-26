50019f8ce98a971511a23835f33cc38e0cf37fa3
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-27 04:07:18 +0530

    Add useRef to wait for the button click to proceed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import { Auth } from "./components/Auth";
 import Cookies from "universal-cookie";
@@ -8,6 +8,8 @@ const cookies = new Cookies();
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
 
   if (!isAuth) {
     return (
@@ -24,8 +26,10 @@ function App() {
       ) : (
         <div className="room">
           <label>Enter Room Name: </label>
-          <input />
-          <button type="">Enter Chat</button>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
         </div>
       )}
     </div>
