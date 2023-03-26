eb2693baa53ca118fbcf611811535d50292a1be6
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-27 03:45:49 +0530

    Add conditional to check whether a user is logged in using useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Auth />
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label>Enter Room Name: </label>
+          <input />
+          <button type="">Enter Chat</button>
+        </div>
+      )}
     </div>
   );
 }
