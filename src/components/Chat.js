cd58d9283364f0fd84c6c7caf1cfb85fbf8d9f76
Thaj Anaz <anaz.thaj@gmail.com>
2023-03-27 23:50:10 +0530

    Add input form for chat with the useState

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Chat() {
-  return <div>Chatty chat chat</div>;
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="chat-app">
+      <form onSubmit={handleSubmit} className="new-message-form">
+        <input
+          className="new-message-input"
+          placeholder="Type your message here..."
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+
+        <button className="sent-button" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Chat;
